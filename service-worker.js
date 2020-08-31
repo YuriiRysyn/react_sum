f6a9888aeb3a6da6edd95c2e68feb021b0d1e427
Yurii Rysyn <zerocool_cs@ukr.net>
2020-08-31 18:48:04 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.0be2a4d51bf87742479c1b1baf41cd4d.js"
+  "./precache-manifest.2dbad53dd6e77d192bd2ae3b4a8e300b.js"
 );
 
 self.addEventListener('message', (event) => {
